2a6e8e4d95703b440c66701b4bb33441bc0e5efd
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-01-15 11:14:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
 import AboutUs from './Components/Aboutus';
-import { render } from "react-dom";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" elements={<Home/>}/>
-          <Route path="/Contact" elements={<Contact/>}/>
-          <Route path="/Aboutus" elements={<AboutUs/>}/>
+          <Route path="/" elements={<Home />} />
+          <Route path="/Contact" elements={<Contact />} />
+          <Route path="/Aboutus" elements={<AboutUs />} />
         </Routes>
       </Router>
-
-      
     </div>
   );
 }
